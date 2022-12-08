85b69d016d5fa654a854f033d43546b747b806f7
JacksonMuriithi <jacksonmuriithi299@mail.com>
2022-12-08 13:32:42 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// code your solution here
+var divide = ( a1,b1) => 20;
+let a1=2000
+let b1=100
+alert(divide(2000,100))
+
+ var square=(x)=> x**2;
+ let x =2
+ alert(square(2));
+
+ var add =(a,b)=>a+b;
+ let a=3
+ let b=4
+ alert(add(3,4));
